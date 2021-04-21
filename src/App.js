96628b12d9416e0bb9e09c977abb4dde1e74898f
Ben <eberleben15@gmail.com>
2021-04-20 20:29:26 -0600

    add ReactStrap

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
 import React from 'react';
+import { Badge, Form, FormGroup, Input, Label } from 'reactstrap';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div class="App">
+      <header>
+        <h1><Badge color="secondary">TheOddsOracle</Badge></h1>
       </header>
+        <body>
+        <Form>
+            <FormGroup>
+                <Label for="exampleSelect">Select Sport</Label>
+                <Input type="select" name="select" id="exampleSelect">
+                    <option>MLB</option>
+                    <option>NBA</option>
+                    <option>NFL</option>
+                    <option>PGA</option>
+                    <option>MLS</option>
+                </Input>
+            </FormGroup>
+        </Form>
+        </body>
     </div>
-  );
+
+
+);
 }
 
 export default App;
