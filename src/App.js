62537c0d24a4ebcfdba7abf9a362656c21cad3c8
Jacce522 <70173113+Jacce522@users.noreply.github.com>
2021-04-21 18:19:43 -0600

    Update App.js
    Updated App.js with function call to odds api which shows up in console.log(); In Chrome go to more tools, developer tools or  Ctrl + Shift + i.
    More work and testing needs to be done yet but making progress.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
 import React from 'react';
-import { Badge, Form, FormGroup, Input, Label } from 'reactstrap';
+import Button, { Badge, Form, FormGroup, Input, Label } from 'reactstrap';
 import logo from './logo.svg';
 import './App.css';
+import  { runOdds } from './Odds.js';
+
+
+
+
+function oddsForGames()
+{
+
+let theOdds = runOdds();
+
+//const url = "https://api.randomuser.me/";
+//const response = await fetch(url);
+//const data = await response.json();
+  //      console.log(data.results[0].name.first);
+  //  alert(data.results[0].name.first + data.results[0].name.last );
+console.log(theOdds);
+
+
+
+
+}
+
+
 
 function App() {
   return (
@@ -12,16 +35,21 @@ function App() {
         <body class="App-body">
         <Form>
             <FormGroup>
-                <Label for="sportSelect">Select Sport</Label>
+                <Label for="sportSelect">Select Sport</Label><br></br>
                 <Input type="select" name="select" id="sportSelect" class="Select-sports">
                     <option>MLB</option>
                     <option>NBA</option>
                     <option>NFL</option>
+                    <option>NHL</option>
                     <option>PGA</option>
                     <option>MLS</option>
                 </Input>
+
             </FormGroup>
         </Form>
+        <br></br>
+        <button onClick={oddsForGames}>Get the Odds</button>
+
         </body>
     </div>
 
